26fb63cfd51a5673708eed163aa1256d75267fd1
Adrian Viquez <aviquez96@gmail.com>
2018-10-18 22:17:54 -0700

    added event handlers; form updates the state when button is pressed without page refreshing, text field updates state when key is pressed

--- countdown/src/components/Countdown/datepicker.js
+++ countdown/src/components/Countdown/datepicker.js
@@ -1,20 +1,45 @@
 import React from 'react'
 
-const Datepicker = (props) => {
-    return (
-        <form action="">
-            <div className="field is-grouped is-grouped-centered" style={{marginBottom: 40}}>
-                <p className="control">
-                    <input className="input is-medium is-rounded" type="text" placeholder="Type a date..."/>
-                </p>
-                <p className="control">
-                    <a className="button is-light is-outlined is-medium is-rounded">
-                    Reset
-                    </a>
-                </p>
-            </div>
-        </form>
-    )
+class Datepicker extends React.Component {
+    state = {
+        date: '' 
+    }
+
+    // Arrow functions preserve the context of the function
+    handleDateChange = (e) => {
+        this.setState({
+            date: e.target.value
+        })
+    }
+
+    handleDateSubmit = (e) => {
+        // Prevents the page from refreshing, which allows the state to be changed when the button is pressed
+        e.preventDefault();
+        console.log(this.state)
+    }
+
+    render () {
+        const {date} = this.state
+
+        return (
+            <form action="" onSubmit={this.handleDateSubmit}>
+                <div className="field is-grouped is-grouped-centered" style={{marginBottom: 40}}>
+                    <p className="control">
+                        <input className="input is-medium is-rounded" 
+                                value={date} 
+                                onChange={this.handleDateChange}
+                                type="text" 
+                                placeholder="Type a date..."/>
+                    </p>
+                    <p className="control">
+                        <button className="button is-light is-outlined is-medium is-rounded">
+                        Reset
+                        </button>
+                    </p>
+                </div>
+            </form>
+        )
+    }
 }
 
 export default Datepicker;
